64f60db34ac84b41081606c0e25e178b77ad6ecc
鄭皓中 <peteryoung@MacBook-Pro.local>
2020-06-08 12:50:35 +0800

    LSTM model

--- exam3/model.py
+++ exam3/model.py
@@ -1,5 +1,65 @@
+import torch
 import torch.nn as nn
+from torch.nn import Parameter
+import torch.optim as optim
 
+from enum import IntEnum
+from typing import *
 
+class Dim(IntEnum):
+    batch = 0
+    seq = 1
+    feature = 2
+
+# Much faster especially on the GPU
 class Model(nn.Module):
-    pass
+    def __init__(self, input_sz: int, hidden_sz: int):
+        super().__init__()
+        self.input_size = input_sz
+        self.hidden_size = hidden_sz
+        self.weight_ih = Parameter(torch.Tensor(input_sz, hidden_sz * 4))
+        self.weight_hh = Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
+        self.bias = Parameter(torch.Tensor(hidden_sz * 4))
+        
+        self.init_weights()
+        
+    def init_weights(self):
+        for p in self.parameters():
+            if p.data.ndimension() >= 2:
+                nn.init.xavier_uniform_(p.data)
+            else:
+                nn.init.zeros_(p.data)
+                
+    def forward(self, x: torch.Tensor,
+                      init_states: Optional[Tuple[torch.Tensor]]=None
+               ) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        """Assumes x is of shape (batch, sequence, feature)"""
+        bs, seq_sz, _ = x.size()
+        hidden_seq = []
+        if init_states is None:
+            h_t, c_t = (torch.zeros(self.hidden_size).to(x.device),
+                        torch.zeros(self.hidden_size).to(x.device))
+        else:
+            h_t, c_t = init_states
+            
+        HS = self.hidden_size
+        for t in range(seq_sz): # iterate over the time steps
+            x_t = x[:, t, :]
+            # batch the computations into a single matrix multiplication
+            gates = x_t @ self.weight_ih + h_t @ self.weight_hh + self.bias
+            
+            i_t, f_t, g_t, o_t = (
+                torch.sigmoid(gates[:, :HS]), # input gate
+                torch.sigmoid(gates[:, HS:HS*2]), # forget gate
+                torch.tanh(gates[:, HS*2:HS*3]), # g
+                torch.sigmoid(gates[:, HS*3:]), # output gate
+            )
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
+            hidden_seq.append(h_t.unsqueeze(Dim.batch))
+            
+        hidden_seq = torch.cat(hidden_seq, dim=Dim.batch)
+        # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
+        hidden_seq = hidden_seq.transpose(Dim.batch, Dim.seq).contiguous()
+        
+        return hidden_seq, (h_t, c_t)

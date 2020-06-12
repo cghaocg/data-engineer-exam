9337a2abe02eec38a5843625ead3381fa6b151e6
鄭皓中 <peteryoung@MacBook-Pro.local>
2020-06-12 17:19:30 +0800

    exam3: 1.model update 2.Still just trained with 'train_need_aggregate.csv'

--- exam3/model.py
+++ exam3/model.py
@@ -1,65 +1,24 @@
 import torch
 import torch.nn as nn
-from torch.nn import Parameter
-import torch.optim as optim
 
-from enum import IntEnum
-from typing import *
-
-class Dim(IntEnum):
-    batch = 0
-    seq = 1
-    feature = 2
-
-# Much faster especially on the GPU
 class Model(nn.Module):
-    def __init__(self, input_sz: int, hidden_sz: int):
+    def __init__(self, input_size:int, hidden_layer_size:int, batch_size:int, output_size:int, num_layers:int):
         super().__init__()
-        self.input_size = input_sz
-        self.hidden_size = hidden_sz
-        self.weight_ih = Parameter(torch.Tensor(input_sz, hidden_sz * 4))
-        self.weight_hh = Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
-        self.bias = Parameter(torch.Tensor(hidden_sz * 4))
+        self.input_size = input_size
+        self.hidden_layer_size = hidden_layer_size
+        self.batch_size = batch_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
         
-        self.init_weights()
-        
-    def init_weights(self):
-        for p in self.parameters():
-            if p.data.ndimension() >= 2:
-                nn.init.xavier_uniform_(p.data)
-            else:
-                nn.init.zeros_(p.data)
-                
-    def forward(self, x: torch.Tensor,
-                      init_states: Optional[Tuple[torch.Tensor]]=None
-               ) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
-        """Assumes x is of shape (batch, sequence, feature)"""
-        bs, seq_sz, _ = x.size()
-        hidden_seq = []
-        if init_states is None:
-            h_t, c_t = (torch.zeros(self.hidden_size).to(x.device),
-                        torch.zeros(self.hidden_size).to(x.device))
-        else:
-            h_t, c_t = init_states
-            
-        HS = self.hidden_size
-        for t in range(seq_sz): # iterate over the time steps
-            x_t = x[:, t, :]
-            # batch the computations into a single matrix multiplication
-            gates = x_t @ self.weight_ih + h_t @ self.weight_hh + self.bias
-            
-            i_t, f_t, g_t, o_t = (
-                torch.sigmoid(gates[:, :HS]), # input gate
-                torch.sigmoid(gates[:, HS:HS*2]), # forget gate
-                torch.tanh(gates[:, HS*2:HS*3]), # g
-                torch.sigmoid(gates[:, HS*3:]), # output gate
-            )
-            c_t = f_t * c_t + i_t * g_t
-            h_t = o_t * torch.tanh(c_t)
-            hidden_seq.append(h_t.unsqueeze(Dim.batch))
-            
-        hidden_seq = torch.cat(hidden_seq, dim=Dim.batch)
-        # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
-        hidden_seq = hidden_seq.transpose(Dim.batch, Dim.seq).contiguous()
-        
-        return hidden_seq, (h_t, c_t)
+        # num_layers, batch_size, self.hidden_size
+        self.hidden_cell = (torch.zeros(num_layers, batch_size, hidden_layer_size),
+                            torch.zeros(num_layers, batch_size, hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) , self.batch_size, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
--- exam3/train.py
+++ exam3/train.py
@@ -1,5 +1,83 @@
+import os
+import pandas as pd
+import numpy as np
+import argparse
+
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import MinMaxScaler
+
 from model import Model
 
 if __name__ == '__main__':
-    model = Model()
-    pass
+    if not os.path.exists("model"):
+        os.mkdir("model")
+    
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Train LSTM model")
+    parser.add_argument("--epochs", type=int, default=10, metavar="N",
+                        help="number of epochs to train (default: 10)")
+    args = parser.parse_args()
+
+
+    #================= data processing =================#
+    df_train_aggr = pd.read_csv("../exam2/data/train_need_aggregate.csv")
+    df_test_aggr = pd.read_csv("../exam2/data/test_need_aggregate.csv")
+
+    df_train = pd.read_csv("../exam2/result/train.csv")
+    df_test = pd.read_csv("../exam2/result/test.csv")
+
+    # Normalization
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    df_train_aggr_norm = scaler.fit_transform(df_train_aggr["EventId"].values.reshape(-1, 1))
+
+    df_train_aggr_norm = torch.FloatTensor(df_train_aggr_norm).view(-1)
+
+    # a pattern cycle approximately
+    train_window = 70
+
+    def create_input_sequences(input_data, tw):
+        input_seq = []
+        L = len(input_data)
+        for i in range(L-tw):
+            train_seq = input_data[i:i+tw]
+            train_label = input_data[i+tw:i+tw+1]
+            input_seq.append((train_seq ,train_label))
+        return input_seq
+
+    train_input_seq = create_input_sequences(df_train_aggr_norm, train_window)
+
+
+    #================= train model =================#
+    input_size = 1
+    hidden_layer_size = 20
+    batch_size = 1
+    output_size = 1
+    num_layers = 1
+
+    model = Model(input_size, hidden_layer_size, batch_size, output_size, num_layers)
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+    epochs = 1
+    
+    for i in range(epochs):
+        for seq, labels in train_input_seq:
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(model.num_layers, model.batch_size, model.hidden_layer_size),
+                                torch.zeros(model.num_layers, model.batch_size, model.hidden_layer_size))
+
+            y_pred = model(seq)
+
+            single_loss = loss_function(y_pred, labels)
+            single_loss.backward()
+            optimizer.step()
+
+        print(f"epoch: {i:3} loss: {single_loss.item():10.8f}")
+    
+        
+    #================= save model weights =================#
+    model_path = "./model/lstm.pkl"
+    torch.save(model.state_dict(), model_path)
+

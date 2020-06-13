2c5ddc7c3293b017f3dda57883c7c3b6093a297c
鄭皓中 <peteryoung@MacBook-Pro.local>
2020-06-13 12:15:13 +0800

    exam2: refined the code, removed .sum() and for loop

--- exam2/data_processing.py
+++ exam2/data_processing.py
@@ -8,37 +8,16 @@ def data_processing():
     df_train["datetime"] = pd.to_datetime(df_train["datetime"])
     df_test["datetime"] = pd.to_datetime(df_test["datetime"])
 
-
-    ## train
-    df_train_new = df_train.copy()
+    ### train
     # 依分鐘 resample
-    df_train_new.index = df_train_new.datetime
-    df_train_new = df_train_new.resample(rule="T").sum().reset_index()
+    df_train.index = df_train.datetime
+    df_train = df_train.resample(rule="T").agg({"EventId": lambda x: x.tolist()}).reset_index()
 
-    df_train_new["EventId"] = df_train_new["EventId"].astype(object)
-
-    # 依分鐘放入所屬的 list
-    for row in df_train_new.itertuples():
-        df_train_new.at[getattr(row, "Index"), "EventId"] = (
-            df_train[(df_train['datetime'] >= getattr(row, "datetime")) & 
-                    (df_train['datetime'] < getattr(row, "datetime") + pd.Timedelta('1 minute'))]["EventId"].values.tolist())
-
-
-    ## test
-    df_test_new = df_test.copy()
+    ### test
     # 依分鐘 resample
-    df_test_new.index = df_test_new.datetime
-    df_test_new = df_test_new.resample(rule="T").sum().reset_index()
-
-    df_test_new["EventId"] = df_test_new["EventId"].astype(object)
-
-    # 依分鐘放入所屬的 list
-    for row in df_test_new.itertuples():
-        df_test_new.at[getattr(row, "Index"), "EventId"] = (
-            df_test[(df_test['datetime'] >= getattr(row, "datetime")) & 
-                    (df_test['datetime'] < getattr(row, "datetime") + pd.Timedelta('1 minute'))]["EventId"].values.tolist())
-
+    df_test.index = df_test.datetime
+    df_test = df_test.resample(rule="T").agg({"EventId": lambda x: x.tolist()}).reset_index()
 
     # 產 .csv
-    df_train_new.to_csv("./result/train.csv", index=False)
-    df_test_new.to_csv("./result/test.csv", index=False) 
+    df_train.to_csv("./result/train.csv", index=False)
+    df_test.to_csv("./result/test.csv", index=False)
